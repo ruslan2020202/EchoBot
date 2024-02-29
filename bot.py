472026febe8d232652320ab5cdbb5fecbe385c16
ruslan2020202 <ruslan020507@gmail.com>
2024-02-29 03:28:07 +0300

    commit

--- bot.py
+++ bot.py
@@ -2,33 +2,21 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, types, filters, F
+
 from config import token_api
 
 bot = Bot(token_api)
 dp = Dispatcher()
-photo_id = None
 
 
 @dp.message(filters.CommandStart())
 async def cdm_start(message: types.Message):
-    await message.answer('Привет')
+    await message.answer('Привет, я ЭхоБот')
 
 
-@dp.message(filters.Command('help'))
-async def cmd_help(message: types.Message):
-    await message.answer('Это команда /help')
-
-
-@dp.message(F.text == 'Как дела?')
-async def how_are_you(message: types.Message):
-    await message.reply('OK!')
-
-
-@dp.message(F.photo)
-async def photo(message: types.Message):
-    global photo_id
-    photo_id = message.photo[-1].file_id
-    await message.reply(f'ID фото: {photo_id}')
+@dp.message(F.text)
+async def echo(message: types.Message):
+    await message.reply(message.text)
 
 
 if __name__ == '__main__':
